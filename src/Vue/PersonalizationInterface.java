de9730edaef089b8c9299a7d6cece730bb921b9b
Malthael <ma.1thea1.wright@gmail.com>
2025-09-30 15:50:37 +0200

    Finalisation de la page de personnalisation

--- src/Vue/PersonalizationInterface.java
+++ src/Vue/PersonalizationInterface.java
@@ -1,5 +1,6 @@
 package Vue;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -13,6 +14,7 @@ import javax.swing.SwingConstants;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JColorChooser;
 
 public class PersonalizationInterface extends JFrame {
 
@@ -20,6 +22,16 @@ public class PersonalizationInterface extends JFrame {
 	private JPanel contentPane;
 	private JComboBox<String> FontComboBox;
 	private JComboBox<String> FontComboBox_1;
+	private JComboBox<Integer> SizeComboBox;
+	private JComboBox<Integer> SizeComboBox_1;
+	private JCheckBox ItalicCheckBox;
+	private JCheckBox BoldCheckBox;
+	private JCheckBox UnderlineCheckBox;
+	private JLabel TextPersonalizationPreview;
+	private JCheckBox ItalicCheckBox_1;
+	private JCheckBox BoldCheckBox_1;
+	private JCheckBox UnderlineCheckBox_1;
+	private JLabel TextPersonalizationPreview_1;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -36,7 +48,50 @@ public class PersonalizationInterface extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	
+	private void updateTitlePreviewFont() {
+		String fontName = (String) FontComboBox.getSelectedItem();
+		int fontSize = (Integer) SizeComboBox.getSelectedItem();
+		int style = Font.PLAIN;
+		if (BoldCheckBox.isSelected()) style |= Font.BOLD;
+		if (ItalicCheckBox.isSelected()) style |= Font.ITALIC;
+		Font font = new Font(fontName, style, fontSize);
+		TextPersonalizationPreview.setFont(font);
+		if (UnderlineCheckBox.isSelected()) {
+			TextPersonalizationPreview.setFont(font.deriveFont(
+				font.getAttributes()
+			));
+			java.util.Map<java.awt.font.TextAttribute, Object> attrs = new java.util.HashMap<>(font.getAttributes());
+			attrs.put(java.awt.font.TextAttribute.UNDERLINE, java.awt.font.TextAttribute.UNDERLINE_ON);
+			TextPersonalizationPreview.setFont(font.deriveFont(attrs));
+		}
+		
+	}
+	
+	private void updateTextPreviewFont() {
+		String fontName = (String) FontComboBox_1.getSelectedItem();
+		int fontSize = (Integer) SizeComboBox_1.getSelectedItem();
+		int style = Font.PLAIN;
+		if (BoldCheckBox_1.isSelected()) style |= Font.BOLD;
+		if (ItalicCheckBox_1.isSelected()) style |= Font.ITALIC;
+		Font font = new Font(fontName, style, fontSize);
+		TextPersonalizationPreview_1.setFont(font);
+		if (UnderlineCheckBox_1.isSelected()) {
+			TextPersonalizationPreview_1.setFont(font.deriveFont(
+				font.getAttributes()
+			));
+			java.util.Map<java.awt.font.TextAttribute, Object> attrs = new java.util.HashMap<>(font.getAttributes());
+			attrs.put(java.awt.font.TextAttribute.UNDERLINE, java.awt.font.TextAttribute.UNDERLINE_ON);
+			TextPersonalizationPreview_1.setFont(font.deriveFont(attrs));
+		}
+		
+	}
+	
 	public PersonalizationInterface() {
+		Integer[] sizes = new Integer[33];
+		for (int i = 0, val = 8; val <= 72; i++, val += 2) {
+			sizes[i] = val;
+		}
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 645, 567);
 		contentPane = new JPanel();
@@ -55,7 +110,7 @@ public class PersonalizationInterface extends JFrame {
 		TextPersonalization.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		contentPane.add(TextPersonalization);
 		
-		JLabel TextPersonalizationPreview = new JLabel("aA");
+		TextPersonalizationPreview = new JLabel("aA");
 		TextPersonalizationPreview.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		TextPersonalizationPreview.setHorizontalAlignment(SwingConstants.CENTER);
 		TextPersonalizationPreview.setBounds(516, 120, 105, 71);
@@ -70,6 +125,7 @@ public class PersonalizationInterface extends JFrame {
 				.getLocalGraphicsEnvironment()
 				.getAvailableFontFamilyNames());
 		FontComboBox.setBounds(83, 102, 159, 30);
+		FontComboBox.addActionListener(e -> updateTitlePreviewFont());
 		contentPane.add(FontComboBox);
 		
 		JLabel FontLabel = new JLabel("Police :");
@@ -77,8 +133,9 @@ public class PersonalizationInterface extends JFrame {
 		FontLabel.setBounds(20, 102, 75, 30);
 		contentPane.add(FontLabel);
 		
-		JComboBox<?> SizeComboBox = new JComboBox<>();
+		SizeComboBox = new JComboBox<>(sizes);
 		SizeComboBox.setBounds(83, 151, 159, 30);
+		SizeComboBox.addActionListener(e -> updateTitlePreviewFont());
 		contentPane.add(SizeComboBox);
 		
 		JLabel SizeLabel = new JLabel("Taille  :");
@@ -93,11 +150,18 @@ public class PersonalizationInterface extends JFrame {
 		
 		JButton ColorButton = new JButton("Choisir le couleur");
 		ColorButton.setBounds(83, 198, 159, 41);
+		ColorButton.addActionListener(e -> {
+			Color chosen = JColorChooser.showDialog(this, "Choisir une couleur", TextPersonalizationPreview.getForeground());
+			if (chosen != null) {
+				TextPersonalizationPreview.setForeground(chosen);
+			}
+		});
 		contentPane.add(ColorButton);
 		
-		JCheckBox ItalicCheckBox = new JCheckBox("Italique");
+		ItalicCheckBox = new JCheckBox("Italique");
 		ItalicCheckBox.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		ItalicCheckBox.setBounds(331, 102, 179, 60);
+		ItalicCheckBox.addActionListener(e -> updateTitlePreviewFont());
 		contentPane.add(ItalicCheckBox);
 		
 		JLabel LabelEffects = new JLabel("Effets  :");
@@ -105,19 +169,22 @@ public class PersonalizationInterface extends JFrame {
 		LabelEffects.setBounds(312, 81, 75, 30);
 		contentPane.add(LabelEffects);
 		
-		JCheckBox BoldCheckBox = new JCheckBox("Gras");
+		BoldCheckBox = new JCheckBox("Gras");
 		BoldCheckBox.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		BoldCheckBox.setBounds(331, 142, 179, 52);
+		BoldCheckBox.addActionListener(e -> updateTitlePreviewFont());
 		contentPane.add(BoldCheckBox);
 		
-		JCheckBox UnderlineCheckBox = new JCheckBox("Souligné");
+		UnderlineCheckBox = new JCheckBox("Souligné");
 		UnderlineCheckBox.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		UnderlineCheckBox.setBounds(331, 179, 179, 52);
+		UnderlineCheckBox.addActionListener(e -> updateTitlePreviewFont());
 		contentPane.add(UnderlineCheckBox);
 		
-		JCheckBox UnderlineCheckBox_1 = new JCheckBox("Souligné");
+		UnderlineCheckBox_1 = new JCheckBox("Souligné");
 		UnderlineCheckBox_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		UnderlineCheckBox_1.setBounds(331, 375, 179, 52);
+		UnderlineCheckBox_1.addActionListener(e -> updateTextPreviewFont());
 		contentPane.add(UnderlineCheckBox_1);
 		
 		JLabel ColorLabel_1 = new JLabel("Couleur  :");
@@ -125,8 +192,20 @@ public class PersonalizationInterface extends JFrame {
 		ColorLabel_1.setBounds(10, 397, 75, 30);
 		contentPane.add(ColorLabel_1);
 		
+		TextPersonalizationPreview_1 = new JLabel("aA");
+		TextPersonalizationPreview_1.setHorizontalAlignment(SwingConstants.CENTER);
+		TextPersonalizationPreview_1.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		TextPersonalizationPreview_1.setBounds(516, 316, 105, 71);
+		contentPane.add(TextPersonalizationPreview_1);
+		
 		JButton ColorButton_1 = new JButton("Choisir le couleur");
 		ColorButton_1.setBounds(83, 394, 159, 41);
+		ColorButton_1.addActionListener(e -> {
+			Color chosen = JColorChooser.showDialog(this, "Choisir une couleur", TextPersonalizationPreview_1.getForeground());
+			if (chosen != null) {
+				TextPersonalizationPreview_1.setForeground(chosen);
+			}
+		});
 		contentPane.add(ColorButton_1);
 		
 		JLabel SizeLabel_1 = new JLabel("Taille  :");
@@ -134,24 +213,28 @@ public class PersonalizationInterface extends JFrame {
 		SizeLabel_1.setBounds(22, 346, 75, 30);
 		contentPane.add(SizeLabel_1);
 		
-		JComboBox<?> SizeComboBox_1 = new JComboBox<>();
+		SizeComboBox_1 = new JComboBox<>(sizes);
 		SizeComboBox_1.setBounds(83, 347, 159, 30);
+		SizeComboBox_1.addActionListener(e -> updateTextPreviewFont());
 		contentPane.add(SizeComboBox_1);
 		
-		JCheckBox BoldCheckBox_1 = new JCheckBox("Gras");
+		BoldCheckBox_1 = new JCheckBox("Gras");
 		BoldCheckBox_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		BoldCheckBox_1.setBounds(331, 338, 179, 52);
+		BoldCheckBox_1.addActionListener(e -> updateTextPreviewFont());
 		contentPane.add(BoldCheckBox_1);
 		
-		JCheckBox ItalicCheckBox_1 = new JCheckBox("Italique");
+		ItalicCheckBox_1 = new JCheckBox("Italique");
 		ItalicCheckBox_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		ItalicCheckBox_1.setBounds(331, 298, 179, 60);
+		ItalicCheckBox_1.addActionListener(e -> updateTextPreviewFont());
 		contentPane.add(ItalicCheckBox_1);
 		
 		FontComboBox_1 = new JComboBox<>(GraphicsEnvironment
 				.getLocalGraphicsEnvironment()
 				.getAvailableFontFamilyNames());
 		FontComboBox_1.setBounds(83, 298, 159, 30);
+		FontComboBox_1.addActionListener(e -> updateTextPreviewFont());
 		contentPane.add(FontComboBox_1);
 		
 		JLabel FontLabel_1 = new JLabel("Police :");
@@ -173,12 +256,6 @@ public class PersonalizationInterface extends JFrame {
 		VisualisationLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		VisualisationLabel_1.setBounds(528, 289, 81, 41);
 		contentPane.add(VisualisationLabel_1);
-		
-		JLabel TextPersonalizationPreview_1 = new JLabel("aA");
-		TextPersonalizationPreview_1.setHorizontalAlignment(SwingConstants.CENTER);
-		TextPersonalizationPreview_1.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		TextPersonalizationPreview_1.setBounds(516, 316, 105, 71);
-		contentPane.add(TextPersonalizationPreview_1);
 		
 		JButton btnNewButton = new JButton("Valider la personnalisation");
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
